f3e80a3be277d4dd37639793e2a501d9b64edbfd
laurarundle <rundlel@tcd.ie>
2016-11-05 17:40:31 +0000

    add command line args and responses to KILL_SERVICE and other messages

--- myserver.js
+++ myserver.js
@@ -1,7 +1,11 @@
 var net = require('net');
 
 var server = new net.createServer();
-server.listen(9000, '10.62.0.140', function(){
+
+var port = process.argv[2];
+var IP = process.argv[3];
+var studentNo = '13321661';
+server.listen(port, IP, function(){
 	console.log("server listening")
 });
 
@@ -10,8 +14,19 @@ server.on('connection', function(socket){
 	socket.setEncoding('utf8');
 
 	socket.on("data", function(message){
-		socket.write('hello');
-		console.log(message);
+		
+		if(message=='KILL_SERVICE\n')
+		{
+			socket.destroy();
+			server.close();
+			console.log("connection closed");
+		}
+		else
+		{
+			socket.write(message + "IP:" + IP + "\nPort:" + port + "\nStudentID:" + studentNo);
+			console.log(message);
+		}
+		
 
 	});
 });
