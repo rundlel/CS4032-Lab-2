dd7736de4ddab95a03aad8d7f1b01a1ac57c7a8d
laurarundle <rundlel@tcd.ie>
2016-11-05 17:58:14 +0000

    fixed issues with IP addressextraction

--- myserver.js
+++ myserver.js
@@ -3,8 +3,10 @@ var net = require('net');
 var server = new net.createServer();
 
 var port = process.argv[2];
-var IP = process.argv[3];
+var IP = 10.62.0.140
+
 var studentNo = '13321661';
+
 server.listen(port, IP, function(){
 	console.log("server listening")
 });
@@ -13,23 +15,28 @@ server.on('connection', function(socket){
 	console.log('connection received');
 	socket.setEncoding('utf8');
 
+	var address = socket.address().address;
+
 	socket.on("data", function(message){
 		
 		if(message=='KILL_SERVICE\n')
 		{
 			socket.destroy();
 			server.close();
-			console.log("connection closed");
 		}
 		else
 		{
-			socket.write(message + "IP:" + IP + "\nPort:" + port + "\nStudentID:" + studentNo);
+			socket.write(message + "IP:" + address + "\nPort:" + port + "\nStudentID:" + studentNo);
 			console.log(message);
 		}
 		
 
 	});
 });
+
+server.on('close', function(){
+	console.log("connection closed");
+})
 
 server.on('error', function(error){
 	console.log('OOOOOPS MY BAD' + error.message);
